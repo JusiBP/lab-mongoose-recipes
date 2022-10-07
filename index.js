1ef4fd3789462d21d1ad5413d1ff297b3a57c23c
JB <josepbardesp@gmail.com>
2022-10-07 16:18:30 +0200

    recipes done

--- index.js
+++ index.js
@@ -15,9 +15,70 @@ mongoose
     // Before adding any recipes to the database, let's remove all existing ones
     return Recipe.deleteMany()
   })
+
+  //Mi receta:
   .then(() => {
-    // Run your code here, after you have insured that the connection was made
+    console.log(`Ceii`);
+    return Recipe.create({
+      // TODO: write the schema
+    title: "Elixir de la vida eterna",
+    level: "UltraPro Chef",
+    ingredients: ["cola de raton", "ancas de rana", "ojete de dragon", "mucosa de trol", "cuerno de unicornio", "pis de paloma común", "corazón de infante humano"],
+    cuisine: "Arcana",
+    dishType: "other",
+    image: "https://cdn.domestika.org/c_fit,dpr_auto,f_auto,t_base_params,w_820/v1590074334/content-items/004/642/771/1_behance-original.jpg?1590074334",
+    duration: 100000000,
+    creator: "Belcebu",
+    created: 06/06/666
+    })
   })
+
+  .then(resultat => {
+    console.log("Recipe: ", resultat.title);
+  })
+
+  .then(() => {
+    return Recipe.insertMany(data)
+  })
+  .then(resultat => {
+    resultat.forEach((recipe)=>{
+      console.log("Recipe: ", recipe.title)
+    })
+  })
+
+  .then(() => {
+    return Recipe.findOneAndUpdate({title: "Rigatoni alla Genovese"},{duration:100})
+  })
+  .then(resultat2 => {
+    console.log("resultat2: PERFECTO, TODO SIGUE IGUAL! MIRA MIRA...", resultat2.duration)
+  })
+
+  .then(() => {
+    return Recipe.deleteOne({title: "Carrot Cake"})
+  })
+  .then(resultat3 => {
+    console.log(resultat3)
+    console.log("Borrado!")
+    return true
+  })
+
+  .then(clossing => {
+    if (clossing){
+      mongoose.connection.close(()=>{
+        console.log("close!");
+      })
+    }
+  })
+
+
   .catch(error => {
     console.error('Error connecting to the database', error);
   });
+
+
+  
+
+
+
+
+
